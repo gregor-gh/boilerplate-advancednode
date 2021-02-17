b097170387283720ea79383f24ce7253d01614d8
gregor-gh <gregor@live.co.uk>
2021-02-17 19:42:35 +0000

    Finished exercises, implemented chat

--- public/client.js
+++ public/client.js
@@ -1,11 +1,27 @@
 $(document).ready(function () {
 
-  /*global io*/
-  let socket = io()
-  
+  /* Global io*/
+  let socket = io();
+
+  socket.on("user", data => {
+    $("#num-users").text(data.currentUsers + " users online");
+    let message = 
+      data.name +
+      (data.connected ? " has joined the chat." : " has left the chat.");
+    $("#messages").append($("<li>").html("<b>" + message + "</b>"))
+  });
+
+  socket.on("chat message", data => {
+    let message = data.name + ": " + data.message;
+    $("#messages").append($("<li>").text(message));
+  });
+
   // Form submittion with new message in field with id 'm'
   $('form').submit(function () {
+    
     var messageToSend = $('#m').val();
+
+    socket.emit("chat message", messageToSend)
 
     $('#m').val('');
     return false; // prevent form submit from refreshing page
--- server.js
+++ server.js
@@ -10,9 +10,13 @@ const auth = require("./auth.js");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const passportSocketIo = require("passport.socketio")
+const MongoStore = require("connect-mongo")(session);
+const store = new MongoStore({url: process.env.MONGO_URI})
 
-
-
+app.use(cors())
 app.set('view engine', 'pug')
 
 fccTesting(app); //For FCC testing purposes
@@ -24,19 +28,53 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
-  cookie: { secure:false }
+  cookie: { secure:false },
+  key: "express.sid",
+  store: store
 }))
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+  io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: "express.sid",
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+  )
 
 myDB(async client => {
   const myDatabase = await client.db("advnode").collection("users");
   routes(app, myDatabase);
   auth(app, myDatabase);
 
-  io.on("connection", socket => {
-    console.log("A user has connected");
+  let currentUsers = 0;
+  io.on('connection', (socket) => {
+    ++currentUsers;
+    io.emit('user', {
+      name: socket.request.user.username,
+      currentUsers,
+      connected: true});
+    console.log('user ' + socket.request.user.username + ' connected');
+
+    socket.on("chat message", message => {
+      io.emit("chat message", {
+        name: socket.request.user.username,
+        message
+      })
+    })
+
+    socket.on("disconnect",() => {
+      --currentUsers;
+      io.emit("user", {
+        name: socket.request.user.username,
+        currentUsers,
+        connected: false})
+    })
   });
 
 }).catch(e => {
@@ -48,7 +86,17 @@ myDB(async client => {
   })
 })
 
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
 
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
